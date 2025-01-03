d3953c77f734dbd4e6747fab695c182de080d1be
Samuel Bonicenha <sambonicenha@gmail.com>
2025-01-03 11:07:44 -0300

    att

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function calcularDiferencaData(dataInicial) {
-    const hoje = new Date();
+    const hoje = new Date(new Date().toISOString().split("T")[0]);
     const dataInicio = new Date(dataInicial);
   
     const diferencaTempo = hoje - dataInicio;
@@ -35,23 +35,35 @@ const diferenca = calcularDiferencaData(data);
 const divNamoro = document.querySelector('.tempo-namoro')
 
 if(diferenca.anos == 1){
-  divNamoro.innerHTML += `${diferenca.anos} ano, `
+  divNamoro.innerHTML += `${diferenca.anos} ano`
 } else {
-  divNamoro.innerHTML += `${diferenca.anos} anos, `
+  divNamoro.innerHTML += `${diferenca.anos} anos`
 }
 
-if(diferenca.meses == 1){
-  divNamoro.innerHTML += `${diferenca.meses} mês, `
+
+if(diferenca.meses == 0){
+  // nada
+} else if(diferenca.meses == 1){
+  divNamoro.innerHTML += `, ${diferenca.meses} mês`
 } else {
-  divNamoro.innerHTML += `${diferenca.meses} meses, `
+  divNamoro.innerHTML += `, ${diferenca.meses} meses`
 }
 
-if(diferenca.dias == 1){
-  divNamoro.innerHTML += `e ${diferenca.dias} dia juntinhos.`
+if(diferenca.dias == 0){
+ // nada
+}
+else if(diferenca.dias == 1){
+  divNamoro.innerHTML += ` e ${diferenca.dias} dia`
 } else {
-  divNamoro.innerHTML += `e ${diferenca.dias} dias juntinhos.`
+  divNamoro.innerHTML += ` e ${diferenca.dias} dia`
 }
 
+divNamoro.innerHTML += ' juntinhos.'
+
+const diasTotaisdiv = document.querySelector('.dias-totais').innerHTML = diferenca.diasTotais
+
+
+// carroussel
 const slider = document.querySelectorAll('.slider')
 const btnPrev = document.getElementById('prev')
 const btnNext = document.getElementById('next')
